e7aa0b3c67c3a236949ee8d72f0cf01abebcb8ab
Murod's Macbook <murodlearns@gmail.com>
2025-06-11 07:35:53 +0900

    feat: develop CarBigCard common component mobile design

--- libs/components/common/CarBigCard.tsx
+++ libs/components/common/CarBigCard.tsx
@@ -32,7 +32,93 @@ const CarBigCard = (props: CarBigCardProps) => {
 	};
 
 	if (device === 'mobile') {
-		return <div>CAR BIG CARD</div>;
+		return (
+			<Stack className="property-big-card-box">
+				<Box
+					component={'div'}
+					className={'card-img'}
+					onClick={() => goCarDetailPage(car?._id)}
+					style={{ backgroundImage: `url(${REACT_APP_API_URL}/${car?.carImages?.[0]})` }}
+				>
+					{car && car?.carRank >= topCarRank && (
+						<div className={'status'}>
+							<DirectionsCarIcon />
+							<span>top</span>
+						</div>
+					)}
+					<div className={'price'}>${formatterStr(car?.carPrice)}</div>
+				</Box>
+				<Box component={'div'} className={'info'}>
+					<strong onClick={() => goCarDetailPage(car?._id)} className={'title'}>
+						{car?.carTitle}
+					</strong>
+					<p className={'desc'}>{car?.carAddress}</p>
+					<div className={'options'}>
+						<div>
+							<SpeedIcon />
+							<span>{car?.carMileage}km</span>
+						</div>
+						<div>
+							<LocalGasStationIcon />
+							<span>{car?.carFuelType}</span>
+						</div>
+						<div>
+							<SettingsIcon />
+							<span>{car?.carTransmission}</span>
+						</div>
+					</div>
+					<div className={'bott'}>
+						<div>
+							{car?.carRent ? <p>Rent</p> : <span>Rent</span>}
+							{car?.carBarter ? <p>Barter</p> : <span>Barter</span>}
+						</div>
+						<div className="buttons-box">
+							<Stack flexDirection={'row'} style={{ width: 'auto' }}>
+								<IconButton
+									color={'default'}
+									sx={{
+										width: '32px',
+										height: '32px',
+										'& svg': {
+											fontSize: '20px',
+										},
+									}}
+								>
+									<RemoveRedEyeIcon />
+								</IconButton>
+								<Typography className="view-cnt">{car?.carViews}</Typography>
+							</Stack>
+							<Stack
+								flexDirection={'row'}
+								style={{ width: 'auto' }}
+								onClick={(e: any) => {
+									e.stopPropagation();
+									likeCarHandler(user, car?._id);
+								}}
+							>
+								<IconButton
+									color={'default'}
+									sx={{
+										width: '32px',
+										height: '32px',
+										'& svg': {
+											fontSize: '22px',
+										},
+									}}
+								>
+									{car?.meLiked && car?.meLiked[0]?.myFavorite ? (
+										<FavoriteIcon style={{ color: '#eb6753' }} />
+									) : (
+										<FavoriteIcon />
+									)}
+								</IconButton>
+								<Typography className="view-cnt">{car?.carLikes}</Typography>
+							</Stack>
+						</div>
+					</div>
+				</Box>
+			</Stack>
+		);
 	} else {
 		return (
 			<Stack className="property-big-card-box">
