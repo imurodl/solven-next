7f01ef3832f2866b7ff6c0e14a6b7918c8f3b331
imurodl <murodlearns@gmail.com>
2025-09-05 04:10:58 +0900

    fix: optimise for seo and google search console configurations

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://solven.uz',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import { appWithTranslation } from 'next-i18next';
 import '../scss/app.scss';
 import '../scss/pc/main.scss';
 import '../scss/mobile/main.scss';
+import { DefaultSeo } from 'next-seo';
 
 const App = ({ Component, pageProps }: AppProps) => {
 	// @ts-ignore
@@ -19,6 +20,15 @@ const App = ({ Component, pageProps }: AppProps) => {
 		<ApolloProvider client={client}>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
+				<DefaultSeo
+					title="Solven – Find Your Next Car"
+					description="Solven helps you browse, compare, and find cars in Uzbekistan with ease."
+					openGraph={{
+						type: 'website',
+						url: 'https://solven.uz',
+						site_name: 'Solven',
+					}}
+				/>
 				<Component {...pageProps} />
 			</ThemeProvider>
 		</ApolloProvider>
